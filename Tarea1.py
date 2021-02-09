14a96ee211758d94ffc86ba50917bae1ca47cc12
Alonso-Landin-Rangel <78627863+Alonso-Landin-Rangel@users.noreply.github.com>
2021-02-09 00:55:02 -0600

    Creado con Colaboratory

---
+++ Tarea1.py
@@ -0,0 +1,43 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled0.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyMYbqpmKPsiSK7GwFZPf+oE",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Alonso-Landin-Rangel/Rep_Alonso_Landin/blob/main/Tarea1.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "ihmj4lZWNHZI"
+      },
+      "source": [
+        "print ('Hola mundo!!!\\n')\r\n",
+        "nombre = input(\"Ingresa tu nombre: \")\r\n",
+        "apellido1 = input(\"\\nIngresa tu primer apellido: \")\r\n",
+        "apellido2 = input(\"\\nIngresa tu segundo apellido: \")\r\n",
+        "print (\"\\nMucho gusto conocerte\", nombre, apellido1, apellido2, \"!!!\")"
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
